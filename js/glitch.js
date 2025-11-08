b8afa392ea2f3f8e1f9ddab6d7ab75284d58a63c
jberronscervantes <jose.berron@cervantes.es>
2025-11-08 01:06:46 +0100

    Delete js directory

--- js/glitch.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.glitch-card').forEach(card => {
-  card.addEventListener('mouseenter', () => {
-    card.style.animation = 'glitch 0.3s infinite';
-  });
-  card.addEventListener('mouseleave', () => {
-    card.style.animation = '';
-  });
-});
