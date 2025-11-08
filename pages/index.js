ef9050298305ee69649ac30da347bd61caf067d2
jberronscervantes <jose.berron@cervantes.es>
2025-11-08 01:07:09 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Experience from "../components/Experience";
+import Education from "../components/Education";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Experience />
+      <Education />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
